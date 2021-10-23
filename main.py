142eea3674abda66691fbfb79958c117649ef9ba
Garcia Lu <gargar070402@gmail.com>
2021-10-23 17:48:04 -0400

    Update router

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/qr_metadata')
+def gen_qr_code_metadata():
+    # TODO: generate qr code
+    pass
+
+
+@app.route('/validate')
+def validate():
+    # TODO: validate qr code
+    pass
+
+
+@app.route('/vaccine', methods=['GET', 'POST'])
+def view_or_add_vaccine_record():
+    # TODO: GET - get a user's vaccine records
+    # TODO: POST - create a user's vaccine records
+    pass
 
 
 if __name__ == '__main__':
