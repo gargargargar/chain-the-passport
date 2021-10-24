0bddce3fbe4a7bcb93d1d7386767a20c190d7711
Garcia Lu <gargar070402@gmail.com>
2021-10-24 05:02:46 -0400

    Update blockchain code

--- etherium.py
+++ etherium.py
@@ -1,26 +1,28 @@
-import json
 from solcx import compile_source
 from web3 import Web3
 
 contract = None
 w3 = None
 
+
 class Address:
     def __init__(self, address=None):
         global w3
         self.address = address or w3.eth.account.create().address
 
+
 class Contract:
     def __init__(self, address=None, abi=None):
-        url = "http://127.0.0.1:8545"
+        url = "http://128.61.31.247:7545"
         global w3
         w3 = Web3(Web3.HTTPProvider(url))
         w3.eth.default_account = w3.eth.accounts[0]
-        if (address is None):
+        if address is None:
             self.newContract()
         else:
             global contract
             contract = w3.eth.contract(address = address, abi = abi)
+
 
     def newContract(self):
         compiled_sol = compile_source(
@@ -60,20 +62,20 @@ def getString(address: str):
     address = Address(address=address)
     return contract.functions.getString(address.address).call() 
 
+
 def setString(address: str, string: str):
     address = Address(address=address)
     tx_hash = contract.functions.updateString(address.address, string).transact()
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-   
+
+
 def initializeBlockchain():
     c = Contract()
 
-if (__name__ == '__main__'): 
+
+if __name__ == '__main__':
     initializeBlockchain()
     
     addr = Address()
     setString(addr.address, "Hello")
     print(getString(addr.address))
-
-
-    
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from flask import Flask, request
 import user
 import json
 import firebase_admin
+import etherium
 
 app = Flask(__name__)
 
+default_app = firebase_admin.initialize_app()
+etherium.initializeBlockchain()
+
+
 @app.route('/')
 def hello_world():
-    w3 = Web3.HTTPProvider(url)
-    return str(w3.isConnected())
+    return "Hello World!"
 
 
 @app.route('/qr_metadata')
@@ -32,8 +36,6 @@ def view_or_add_vaccine_record():
     # TODO: POST - create a user's vaccine records
     return 'Work in progress'
 
-
-default_app = firebase_admin.initialize_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
